ab8fb83a7b9484ccc4eba1bc1490b8e10a0866de
Abzal Myrzash <abzal.myrzash006@gmail.com>
2025-06-01 17:52:10 +0500

    added deep copying support

--- dynarr.h
+++ dynarr.h
@@ -5,11 +5,12 @@
 
 // a struct that holds pointers to functions that operate on an array's elements
 typedef struct {
-	void (*print)(void*, int); // ptr to func for printing all elements
-	void (*free)(void*); // ptr to func for freeing individual element pointers
+	void (*print)(void*, int); // func for printing elements
+	void* (*copy)(void*); // func for deep copying elements
+	void (*free)(void*); // func for freeing nested pointers
 } DynArrFunc;
 
-DynArrFunc DynArrFuncNULL = { NULL, NULL };
+DynArrFunc DynArrFuncNULL = { NULL, NULL, NULL };
 
 void DynArrInt_print(void* data, int len) {
 	for (int i = 0; i < len; i++) {
@@ -18,20 +19,27 @@ void DynArrInt_print(void* data, int len) {
 	printf("\n");
 }
 
-DynArrFunc DynArrIntFunc = { DynArrInt_print, NULL };
+DynArrFunc DynArrIntFunc = { DynArrInt_print, NULL, NULL };
 
 void DynArrString_print(void* data, int len) {
 	for (int i = 0; i < len; i++) {
 		printf("%s\n", *((char**)data + i));
-		// printf("%s\n", *((char**)data + len));
 	}
+}
+
+void* DynArrString_copy(void* ptr) {
+	char** strptr = ptr;
+	char** copyptr = malloc(sizeof(char*));
+	*copyptr = malloc(strlen(*strptr));
+	strcpy(*copyptr, *strptr);
+	return copyptr;
 }
 
 void DynArrString_free(void* ptr) {
 	free(*(char**)ptr);
 }
 
-DynArrFunc DynArrStringFunc = { DynArrString_print, DynArrString_free };
+DynArrFunc DynArrStringFunc = { DynArrString_print, DynArrString_copy, DynArrString_free };
 
 typedef struct {
 	int len;
@@ -63,17 +71,56 @@ DynArr* DynArr_new(int elem_size, int alloc_size, DynArrFunc func) {
 	return a;
 }
 
+void _DynArr_check_index(DynArr *a, int index) {
+	if (index < 0 || index >= a->len) {
+		printf("ERROR: index out of bounds!\n");
+		exit(1);
+	}
+}
+
+void _DynArr_check_range(DynArr *a, int index, int n) {
+	if (index < 0 || index + n < a->len) {
+		printf("ERROR: Invalid range (i=%d, n=%d) for array (%d)!\n",
+			index, n, a->len);
+		exit(1);
+	}
+}
+
 void* DynArr_at(DynArr* a, int index) {
+	_DynArr_check_index(a, index);
 	return a->data + index * a->elem_size;
 }
 
 void* DynArr_assign(DynArr* a, int index, void* data) {
-	return memcpy(DynArr_at(a, index), a->data, a->elem_size);
+	_DynArr_check_index(a, index);
+	if(a->func.copy != NULL) {
+		data = a->func.copy(data);
+	}
+	void* ptr = memcpy(DynArr_at(a, index), data, a->elem_size);
+	if(a->func.copy != NULL) {
+		free(data);
+	}
+	return ptr;
+}
+
+void* DynArr_assign_arr(DynArr* a, int index, void* arr, int n) {
+	_DynArr_check_range(a, index, n);
+	for(int i = 0; i < n; i++) {
+		DynArr_assign(a, index+i, arr + i * a->elem_size);
+	}
+	return DynArr_at(a, index);
 }
 
 void DynArr_iterate(DynArr *a, void (*f)(void*)) {
 	for(int i = 0; i < a->len; i++) {
 		(*f)(DynArr_at(a, i));
+	}
+}
+
+void DynArr_iterate_range(DynArr *a, int index, int n, void (*f)(void*)) {
+	_DynArr_check_range(a, index, n);
+	for(int i = 0; i < n; i++) {
+		(*f)(DynArr_at(a, index+i));
 	}
 }
 
@@ -84,11 +131,21 @@ void DynArr_print(DynArr* a) {
 }
 
 void DynArr_free(DynArr* a) {
-	if(a == NULL) return;
 	if(a->func.free != NULL) {
 		DynArr_iterate(a, a->func.free);
 	}
+	free(a->data);
 	free(a);
+}
+
+void _DynArr_free_elem(DynArr* a, int index) {
+	if (a->func.free == NULL) return;
+	a->func.free(DynArr_at(a, index));
+}
+
+void _DynArr_free_range(DynArr* a, int index, int n) {
+	if (a->func.free == NULL) return;
+	DynArr_iterate_range(a, index, n, a->func.free);
 }
 
 void DynArr_grow(DynArr *a, int n) {
@@ -105,7 +162,7 @@ void DynArr_grow(DynArr *a, int n) {
 
 void DynArr_shrink(DynArr *a, int n) {
 	if (a->len < n) {
-		printf("Can't remove %d elements from array of size %d!\n", n, a->len);
+		printf("ERROR: Can't shrink array (%d) by %d!\n", a->len, n);
 		exit(1);
 	}
 	a->len -= n;
@@ -114,61 +171,46 @@ void DynArr_shrink(DynArr *a, int n) {
 void* DynArr_append(DynArr *a, void* elem) {
 	int prev_len = a->len;
 	DynArr_grow(a, 1);
-	return memcpy(DynArr_at(a, prev_len), elem, a->elem_size);
+	return DynArr_assign(a, prev_len, elem);
 }
 
 void* DynArr_append_arr(DynArr *a, void* arr, int n) {
 	int prev_len = a->len;
 	DynArr_grow(a, n);
-	return memcpy(DynArr_at(a, prev_len), arr, n * a->elem_size);
+	return DynArr_assign_arr(a, prev_len, arr, n);
 }
 
 void* DynArr_insert(DynArr *a, int ind, void* elem) {
-	if(ind < 0 || ind > a->len) {
-		printf("Can't insert, index (%d) out of bounds [0, %d]!\n", ind, a->len);
-		exit(1);
-	}
+	_DynArr_check_index(a, ind);
 	int prev_len = a->len;
 	DynArr_grow(a, 1);
 	int num_to_move = prev_len - ind;
 	memmove(DynArr_at(a, ind+1), DynArr_at(a, ind), num_to_move * a->elem_size);
-	return memcpy(DynArr_at(a, ind), elem, a->elem_size);
+	return DynArr_assign(a, ind, elem);
 }
 
 void* DynArr_insert_arr(DynArr *a, int ind, void* arr, int n) {
-	if(ind < 0 || ind > a->len) {
-		printf("Can't insert, index (%d) out of bounds [0, %d]!\n", ind, a->len);
-		exit(1);
-	}
+	_DynArr_check_range(a, ind, n);
 	int prev_len = a->len;
 	DynArr_grow(a, n);
 	int num_to_move = prev_len - ind;
 	memmove(DynArr_at(a, ind+n), DynArr_at(a, ind), num_to_move * a->elem_size);
-	return memcpy(DynArr_at(a, ind), arr, n * a->elem_size);
+	return DynArr_assign_arr(a, prev_len, arr, n);
 }
 
 void* DynArr_remove(DynArr *a, int ind) {
-	if(ind < 0 || ind >= a->len) {
-		printf("Can't remove, index (%d) out of bounds [0, %d)!\n", ind, a->len);
-		exit(1);
-	}
+	_DynArr_check_index(a, ind);
 	int prev_len = a->len;
+	_DynArr_free_elem(a, ind);
 	DynArr_shrink(a, 1);
 	int num_to_move = prev_len - ind;
 	return memmove(DynArr_at(a, ind), DynArr_at(a, ind+1), num_to_move * a->elem_size);
 }
 
 void* DynArr_remove_range(DynArr *a, int ind, int n) {
-	if(ind < 0 || ind >= a->len) {
-		printf("Can't remove, index (%d) out of bounds [0, %d)!\n", ind, a->len);
-		exit(1);
-	}
-	if (ind + n > a->len) {
-		printf("Can't remove %d elements from array of size %d starting from index %d!\n",
-			n, a->len, ind);
-		exit(1);
-	}
+	_DynArr_check_range(a, ind, n);
 	int prev_len = a->len;
+	_DynArr_free_range(a, ind, n);
 	DynArr_shrink(a, n);
 	int num_to_move = prev_len - ind;
 	return memmove(DynArr_at(a, ind), DynArr_at(a, ind+n), num_to_move * a->elem_size);
@@ -190,5 +232,9 @@ int DynArr_index(DynArr *a, void* ptr) {
 
 void DynArr_delete(DynArr *a, void* ptr) {
 	int index = DynArr_index(a, ptr);
+	if (index < 0 || index >= a->len) {
+		printf("ERROR: failed to delete, ptr outside dynamic array!\n");
+		exit(1);
+	}
 	DynArr_remove(a, index);
 }
